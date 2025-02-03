168bb18d771e9efe33fa567edb9ca242234e6ad0
ritwnxidig <ritwanrashit@gmail.com>
2025-02-03 09:27:31 +0300

    refactor email age checker to database connection handler with updated tests

--- boot-dot-dev-practices/main_test.go
+++ boot-dot-dev-practices/main_test.go
@@ -2,107 +2,43 @@ package main
 
 import (
 	"fmt"
-	"slices"
 	"testing"
-	"time"
 )
 
 func Test(t *testing.T) {
 	type testCase struct {
-		emails [3]email
-		isOld  [3]bool
+		numDBs int
 	}
 	tests := []testCase{
-		{[3]email{
-			{
-				body: "Words are pale shadows of forgotten names. As names have power, words have power.",
-				date: time.Date(2019, 2, 0, 0, 0, 0, 0, time.UTC),
-			},
-			{
-				body: "It's like everyone tells a story about themselves inside their own head.",
-				date: time.Date(2021, 3, 1, 0, 0, 0, 0, time.UTC),
-			},
-			{
-				body: "Bones mend. Regret stays with you forever.",
-				date: time.Date(2022, 1, 2, 0, 0, 0, 0, time.UTC),
-			},
-		}, [3]bool{true, false, false}},
-		{[3]email{
-			{
-				body: "Music is a proud, temperamental mistress.",
-				date: time.Date(2018, 0, 0, 0, 0, 0, 0, time.UTC),
-			},
-			{
-				body: "Have you heard of that website Boot.dev?",
-				date: time.Date(2017, 0, 0, 0, 0, 0, 0, time.UTC),
-			},
-			{
-				body: "It's awesome honestly.",
-				date: time.Date(2016, 0, 0, 0, 0, 0, 0, time.UTC),
-			},
-		}, [3]bool{true, true, true}},
+		{1},
+		{3},
+		{4},
 	}
 	if withSubmit {
 		tests = append(tests, []testCase{
-			{[3]email{
-				{
-					body: "I have stolen princesses back from sleeping barrow kings.",
-					date: time.Date(2019, 0, 0, 0, 0, 0, 0, time.UTC),
-				},
-				{
-					body: "I burned down the town of Trebon",
-					date: time.Date(2019, 6, 6, 0, 0, 0, 0, time.UTC),
-				},
-				{
-					body: "I have spent the night with Felurian and left with both my sanity and my life.",
-					date: time.Date(2022, 7, 0, 0, 0, 0, 0, time.UTC),
-				},
-			}, [3]bool{true, true, false}},
+			{0},
+			{13},
 		}...)
 	}
 
-	passCount := 0
-	failCount := 0
-
 	for _, test := range tests {
-		isOld := checkEmailAge(test.emails)
-		if !slices.Equal(isOld[:], test.isOld[:]) {
-			failCount++
-			t.Errorf(`
----------------------------------
-Test Failed:
-  input:
-    * %v | %v
-    * %v | %v
-    * %v | %v
-  expected: %v
-  actual:   %v
-`,
-				test.emails[0].body, test.emails[0].date,
-				test.emails[1].body, test.emails[1].date,
-				test.emails[2].body, test.emails[2].date,
-				test.isOld, isOld)
-		} else {
-			passCount++
-			fmt.Printf(`
----------------------------------
-Test Passed:
-  input:
-    * %v | %v
-    * %v | %v
-    * %v | %v
-  expected: %v
-  actual:   %v
-`,
-				test.emails[0].body, test.emails[0].date,
-				test.emails[1].body, test.emails[1].date,
-				test.emails[2].body, test.emails[2].date,
-				test.isOld, isOld)
+		fmt.Printf("---------------------------------\n")
+		fmt.Printf("Testing %v Databases...\n\n", test.numDBs)
+		dbChan, count := getDBsChannel(test.numDBs)
+		waitForDBs(test.numDBs, dbChan)
+		for *count != test.numDBs {
+			fmt.Println("...")
 		}
+		fmt.Printf(`
+expected length: 0, count: %v
+actual length:   %v, count: %v
+PASS
+---------------------------------
+`,
+			test.numDBs, len(dbChan), *count)
 	}
-	fmt.Println("---------------------------------")
-	fmt.Printf("%d passed, %d failed\n", passCount, failCount)
 }
 
-// withSubmit is set at compile time depending on which button is used to run the tests
+// withSubmit is set at compile time depending
+// on which button is used to run the tests
 var withSubmit = true
